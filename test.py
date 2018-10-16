c360a31903f86b61361d77ec0f195cc9403f13dd
Kegan Holtzhausen <marzubus@gmail.com>
2018-10-16 13:59:20 +0200

    test

--- test.py
+++ test.py
@@ -2,8 +2,9 @@ from rasa_nlu.model import Interpreter
 import json
 
 interpreter = Interpreter.load("./models/current/nlu")
-messages = ["the password could be bananas", "username foo, password: Zzzsa23"]
+messages = ["the password could be bananas", "username foo, password: Zzzsa23", "the weather is just fine"]
 
 for message in messages:
     result = interpreter.parse(message)
+    print(result['intent'])
     print(json.dumps(result, indent=2))
--- train.md
+++ train.md
@@ -1,10 +1,10 @@
-## intent:passwy
+## intent:containspassword
 - the password is [Chck22](password)
 - user: [keghol](user) password: [zubzub223](password)
 - login with user id [marvin](user) and password [vookdsa2](password)
 - the password is [password](password)
 
-## intent:nopasswy
+## intent:nopassword
 - the rain in spain, falls mainly on the plains
 - the quick red fox jumped over the lazy brown dog.
 - It's nice out. 
